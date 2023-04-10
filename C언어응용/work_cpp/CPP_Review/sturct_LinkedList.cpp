1a03ddee70c0f5547a31a5175efdce698819aecf
butter3415 <122880490+butter3415@users.noreply.github.com>
2023-04-10 23:48:52 +0900

    C언어 응용 Union_공용체

--- C언어응용/work_cpp/CPP_Review/sturct_LinkedList.cpp
+++ C언어응용/work_cpp/CPP_Review/sturct_LinkedList.cpp
@@ -1,33 +1,33 @@
-#include <iostream>
-using namespace std;
-
-/*
-a, b, c 구조체 노드가 연결되어 있는 구조
-(환형 형태의 LinkedList)
-a -> b -> c
-*/
-
-struct LinkedList {
-
-	int data;
-	LinkedList* p;
-
-};
-
-
-int main() {
-	LinkedList a, b, c;
-	a.data = 99;
-	a.p = &b;
-
-	b.data = 87;
-	b.p = &c;
-
-	c.data = 75;
-	c.p = &a;
-
-	cout << a.data << " = " << c.p->data << " = " << (*c.p).data << " = " << c.p->p->p->p->data << endl;
-	cout << b.data << " = " << a.p->data << " = " << (*a.p).data << " = " << a.p->p->p->p->data << endl;
-	cout << c.data << " = " << b.p->data << " = " << (*b.p).data << " = " << b.p->p->p->p->data << endl;
-
-}
+//#include <iostream>
+//using namespace std;
+//
+///*
+//a, b, c 구조체 노드가 연결되어 있는 구조
+//(환형 형태의 LinkedList)
+//a -> b -> c
+//*/
+//
+//struct LinkedList {
+//
+//	int data;
+//	LinkedList* p;
+//
+//};
+//
+//
+//int main() {
+//	LinkedList a, b, c;
+//	a.data = 99;
+//	a.p = &b;
+//
+//	b.data = 87;
+//	b.p = &c;
+//
+//	c.data = 75;
+//	c.p = &a;
+//
+//	cout << a.data << " = " << c.p->data << " = " << (*c.p).data << " = " << c.p->p->p->p->data << endl;
+//	cout << b.data << " = " << a.p->data << " = " << (*a.p).data << " = " << a.p->p->p->p->data << endl;
+//	cout << c.data << " = " << b.p->data << " = " << (*b.p).data << " = " << b.p->p->p->p->data << endl;
+//
+//}
