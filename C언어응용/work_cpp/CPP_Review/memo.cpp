f9acd3505d65435683d2f55a5c90a71478b99e6f
butter3415 <122880490+butter3415@users.noreply.github.com>
2023-04-11 21:43:01 +0900

    Ｃ언어응용 ｅｎｕｍ１

--- C언어응용/work_cpp/CPP_Review/memo.cpp
+++ C언어응용/work_cpp/CPP_Review/memo.cpp
@@ -1,230 +1,133 @@
+//// Old Style
+
 //#include <iostream>
 //using namespace std;
 //
+//// namespace 를 통해서 별도의 물리적인 공간을 만들어서 해당 변수가 개입하지 못하도록 하기
+//// 다른 폴더를 만들어서 같은 이름의 PASS, FAIL 이어도 충돌이 나지 않도록 하는 것
 //
-//// 클래스 정의  (데이터 멤버와 멤버 함수 선언)
-//class Circle {
-//private:
-//	double radius;
-//	//double mutable radius;
-//	// mutable -> set으로밖에 변경 불가능한 private 멤버 변수를 변경하도록 강제함
-//
-//	static int counts;	// 정적 변수 선언
-//public:
-//	Circle();	// default Sonstructor
-//	Circle(double radius);	// parameter constructor 매개변수 생성자를 만들었기 때문에 작성
-//	// Circle(const Circle& circle);	// Copy Constructor		// Circle 객체가 와야한다.
-//	Circle(const Circle& circle) = delete;	// Copy Constructor // 임의로 변경하지 않도록 동작시킨다.		
-//
-//	~Circle();	// destructor
-//
-//	double getRadius() const;
-//	double getArea() const;
-//	double getPerimeter() const;
-//	void setRadius(double value);
-//
-//	static int getCount();
-//};
-//
-//int Circle::counts = 0;	// 정적 변수 
-//
-////Circle::Circle() {	// 기본 생성자
-////	radius = 1.0;
-////	cout << this << "객체 생성(기본생성자)" << endl << endl;
-////}
-//
-//Circle::Circle() : radius(1.0) {	// 기본 생성자 _ 초기화 리스트
-//	counts++;
-//	cout << this << "객체 생성(기본생성자)" << endl << endl;
+//namespace sophomore {
+//	enum Java { PASS, FAIL };
 //}
 //
-//// -------------------------------------------------------------------------------------------------------------------
-//
-////Circle::Circle(double r) {
-////	radius = r;
-////	cout << "반지름이 " << radius << "인 객체 생성(매개 변수 생성자)" << endl << endl;
-////}
-//
-////Circle::Circle(double radius) {
-////  radius = radius; => 멤버 변수와 매개 변수 구분 불가
-////	this->radius = radius;		// 매개변수를 가져올때는 이름 정확히! 하면 멤버 변수와 충돌 => 'this->' 추가해주기/
-////	cout << "반지름이 " << radius << "인 객체 생성(매개 변수 생성자)" << endl << endl;
-////}
-//
-//Circle::Circle(double radius) : radius(radius) {	// 초기화 변수 추가 // ': 멤버변수(매개변수)'
-//	counts++;
-//	cout << "반지름이 " << radius << "인 객체 생성(매개 변수 생성자)" << endl << endl;
+//namespace freshman {
+//	enum Python { PASS, FAIL };
 //}
 //
-//
-//// -------------------------------------------------------------------------------------------------------------------
-//
-////Circle::Circle(const Circle& c) {
-////	//this->radius = c.radius;
-////	radius = c.radius;
-////	cout << this << " 객체 생성자(복사생성자)" << endl;
-////}
-//
-//Circle::Circle(const Circle& circle)
-//	: radius(circle.radius) {
-//	cout << this << " 객체 생성자(복사생성자)" << endl;
-//}
-//
-//
-//// -------------------------------------------------------------------------------------------------------------------
-//
-//Circle::~Circle() {
-//	counts--;
-//	cout << this << " 객체 소멸. 내가 너를 기억하마!" << endl;
-//}
-//
-//// -------------------------------------------------------------------------------------------------------------------
-//
-//
-//// 멤버 함수 정의 (멤버 함수들은 클래스 선언 부분에서 선언)
-//
-//// getRadius 멤버 함수 정의
-//double Circle::getRadius() const {
-//	// radius = 5.0;	// const member function
-//	// mutable 사용 시 강제로 변수 변경 가능
-//	return radius;
-//}
-//
-//// getArea 멤버 함수 정의
-//double Circle::getArea() const {
-//	const double PI = 3.14;
-//	return (PI * radius * radius);
-//}
-//
-//// getPerimeter 멤버 함수 정의
-//double Circle::getPerimeter() const {
-//	const double PI = 3.14;
-//	return (2 * PI * radius);
-//}
-//
-//// setRadius 멤버 함수 정의
-//void Circle::setRadius(double r) {
-//	radius = r;
-//}
-//
-//int Circle::getCount() {
-//	return counts;
-//}
-//
-//
-//// 일반 함수
-//void test() {
-//	cout << endl;
-//	cout << "Circle4 객체 생성";
-//	Circle circle4(9.0);
-//	cout << "반지름 : " << circle4.getRadius() << endl;
-//
-//	cout << "Circle5 객체 생성";
-//	Circle circle5(10.5);
-//	cout << "반지름 : " << circle5.getRadius() << endl;
-//}
-//
-//
-//// 애플리케이션 부분 (클래스를 인스턴스화해서 객체를 만들고 활용)
 //int main() {
 //
-//	//long test;
-//	//cout << sizeof(test) << endl;	// 4 (윈도우에선 4바이트, 리눅스, 맥에선 8바이트)
 //
-//	// 첫번째 circle 객체를 만들고 멤버 함수 호출
-//	cout << "Circle1 객체 생성" << endl;
-//	Circle circle1;
-//	circle1.setRadius(10.0);
-//	cout << "Circle1 반지름 : " << circle1.getRadius() << endl;
-//	cout << "Circle1 넓이 : " << circle1.getArea() << endl;
-//	cout << "Circle1 둘레 : " << circle1.getPerimeter() << endl;
-//
-//	cout << Circle::getCount() << endl;
-//
-//	test();
-//
-//	cout << endl;
-//
-//	// 두번째 circle 객체를 만들고 멤버 함수 호출
-//	cout << "Circle2 객체 생성" << endl;
-//	Circle circle2;
-//	circle2.setRadius(20.0);
-//	cout << "Circle2 반지름 : " << circle2.getRadius() << endl;
-//	cout << "Circle2 넓이 : " << circle2.getArea() << endl;
-//	cout << "Circle2 둘레 : " << circle2.getPerimeter() << endl;
-//
-//	cout << endl;
-//
-//
-//
-//	// 세번째 circle 객체를 만들고 멤버 함수 호출(매개 변수 생성자 사용)
-//	cout << "Circle3 객체 생성" << endl;
-//	Circle circle3(25.0);
-//	cout << "Circle3 반지름 : " << circle3.getRadius() << endl;
-//	cout << "Circle3 넓이 : " << circle3.getArea() << endl;
-//	cout << "Circle3 둘레 : " << circle3.getPerimeter() << endl;
-//
-//	cout << endl;
-//
-//	cout << "Cricle6 객체 생성" << endl;
-//	// Circle circle6(circle2);
-//	// cout << "Circle6 반지름 : " << circle6.getRadius() << endl;
-//	// circle 타입으로 가져왔다. const로 작성되어 있기 때문에 변경 불가
-//
-//	cout << endl;
 //
 //}
+
+// =========================================================================================
 //
-///*
-//*** const ***
-//: 상수처럼 동작
-//: 사이드 이펙트 발생하지 않음을 보증 (변수 값이 바뀌지 않는다.)
-//*/
+//// Modern Style(enum class c++11)
 //
-///*
-//객체 지향 프로그래밍
-//- 클래스 정의 (속성과 행위 선언)
-//- 멤버 함수 정의 (행위 정의)
-//- 애플리케이션 (객체를 인스턴스화하고 사용)
-//*/
+//#include <iostream>
+//using namespace std;
 //
-///*
-//접근 제한자 : 접근 권한을 나타낼 때 사용
-//=> private, public, protected
-//=> 기본적으로 private 접근 제한자 붙는다.
+//// enum class 로 작성해서 사용 가능
+//// namespace 를 사용하면 폴더를 새로 만들어야 하는 반면 enum class 는 간단하게 사용 가능
 //
-//□ 데이터 멤버와 접근 제한자
-//: 데이터 멤버 -> private 적용
-//: 강조하기 위해 private 붙여서 사용
+//// enum 까지 작성했을 때는 묵시적 변환이 이루어지는 등 약한 결합이 나타난다 (정수형 타입의)
+//// 그러나 enum class 로 묶게 되면 강한 결합이 이루어져 변환이 불가능하다 
+//// => enum 묶으면 case 0 : 사용 가능 /// enum class 묶으면 case 0 : 사용 불가!  => case Cplusplus::PASS : 형식으로 작성해야함
+//// enum class -> 반드시 열거형 타입의 변수 이용해야 한다!
+//// 암시적, 묵시적 변환을 막을 수 있다.
 //
-//□ 멤버 함수와 접근 제한자
-//: 멤버 함수 -> public 적용
-//: 클래스 내부에서만 활용할 경우 -> private
-//*/
-//
-///*
-//*** 멤버 함수(메소드)의 정의 ***
-//1) 멤버 함수에는 한정자(const)가 있다.
-//2) 멤버 함수 앞에 클래스 이름이 붙는다.
-//클래스::이름
-//
-//＃ 함수의 리턴 자료형이 언제나 앞에 위치함
-//*/
-//
-///*
-//*** 인라인 함수 ***
-//클래스 정의 내부에 함수를 정의 -> 인라인 함수
-//*/
-//
-///*
-//*** 애플리케이션 ***
-//- 객체 인스턴스화(객체 만들기) Ex. Circle circle1;
-//*/
+//enum class Java { PASS, FAIL };
+//enum class Python { PASS, FAIL };
+//enum class Cplusplus { PASS, FAIL };
 //
 //
-//// https://en.cppreference.com/w/cpp/language/types
+//int main() {
+//	Cplusplus c = Cplusplus::PASS;
 //
-//// 소멸자 오버로딩 불가능
-//// 소멸자 : ~Circle();
-//// - 결국 죽으니까 오버로딩 불가능
-//// - 가장 먼저 만들어진 객체는 마지막에 소멸된다. -> 만들어진거에 역순으로 소멸된다.
+//	switch (c) {
+//		case 0 :
+//			cout << "합격!";
+//			break;
+//		case Cplusplus::FAIL :
+//			cout << " 재수강!";
+//	}
+//
+//
+//}
+
+// =========================================================================================
+
+// reference (변수의 별명)
+// 변수가 이름을 하나 더 가짐
+// 포인터와 유사한 파생형, 차이는 발생
+// 자료타입& 변수이름 = 대상변수;	// => 복사 생성자 (const Cicle& c);
+// const 변수 / 변수 const / const 변수 const => 방식과 동일
+
+// 레퍼런스는 별도의 메모리 할당을 받지 않고 참조만 한다.
+// 포인터는 별도의 메모리를 할당 받아서 실행 시킨다.
+
+// rule1. 반드시 선언하는 시점에 초기화 되어야 한다.
+// rule2. 처음 정의 외에는 다른 참조 사용 불가
+//
+//
+//#include <iostream>
+//using namespace std;
+//
+//int main() {
+//	// 자료타입& 변수이름 = 대상변수;
+//	//short s = 5;
+//
+//	//short& rs = s;	// 레퍼런스 변수
+//	//short* ps = &s; // 포인터
+//
+//	//cout << s << " " << &s << endl;		// 5 00000020CB2FFB54
+//	//cout << rs << " " << &rs << endl;	// 5 00000020CB2FFB54	// s와 주소 동일
+//	//cout << *ps << " " << &ps << endl;	// 5 00000020CB2FFB98
+//
+//	//int a = 1; 
+//	//int b = 2;
+//	//int& c = a;	// rule 1 (초기화)	
+//
+//	//c = 3;
+//	//c = b;	// rule 2	// b를 참조 하는 것이 아니라 b의 값을 대입한다.
+//	//c = 4;
+//
+//	//cout << a << endl;
+//		
+//	/// [c의 변화] int& c = a;    c = 3;    c = b;    c = 4;
+//	///              1      ->      3    ->   2   ->    4
+//	/// [a의 변화] int& c = a;    c = 3;    c = b;    c = 4;
+//	///              1      ->      3    ->   2   ->    4
+//	/// [b의 변화]                          c = b;    c = 4;
+//	///                                       2   ->    2      <- c 값 변경에 따른 영향 x
+//
+//	// =========================================================================================
+//
+//	//// const 레퍼런스 변수 : 값 변경 X
+//	////					   : 상수 초기화 가능
+//	//// 함수와 함께 사용할때 시너지 효과 up!
+//
+//	//int a = 1;
+//	//int b = 2;
+//	//const int& c = a;	// rule 1 (초기화)	
+//
+//	//// c = 3;	// 오류 -> const 레퍼런스 변수는 변경 불가
+//	//// c = b;	// 오류 -> const 레퍼런스 변수는 변경 불가
+//	//// c = 4;	// 오류 -> const 레퍼런스 변수는 변경 불가
+//
+//	//cout << c << endl;
+//	//
+//	//a = 5; 
+//
+//	//cout << c << endl;	// 참조는 계속 하므로 참조하고 있던 a 가 바뀌면 c 참조값도 변경되어 나옴
+//
+//	int a = 5;
+//	// int& ra = 9;		// 불가
+//	const int& ra = 9;	// 가능 ( 9 참조 )	->  수정 불가
+//
+//	cout << ra << endl;
+//
+//	// 레퍼런스 변수의 대상 변수는 변수 이름이 와야 정상
+//	// 그러나 const 를 붙이면 값으로 대상 변수 역할 가능	
+//
+//}
