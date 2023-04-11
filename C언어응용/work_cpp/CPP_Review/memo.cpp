95feb0da1dc29810a57167ad5fcf3e7ecaa0f0c1
butter3415 <122880490+butter3415@users.noreply.github.com>
2023-04-11 22:34:38 +0900

    C언어응용 reference 변수

--- C언어응용/work_cpp/CPP_Review/memo.cpp
+++ C언어응용/work_cpp/CPP_Review/memo.cpp
@@ -1,133 +1,96 @@
-//// Old Style
-
+//// 함수 : 기능, 행위, Function, 값을 던져주면 처리해서 리턴하는 역할
+//// 메인함수 위쪽, 아래쪽 위치 가능
+//// 위에서부터 컴파일러가 읽어 가기 때문에 메인 함수보다 Hello() 함수가 더 아래 있으면 오류
+//// 보통 메인 함수 아래에 함수를 작성하고 위에 프로토 타입 작성하면서 함수를 설명한다.
+//
+////#include <iostream>
+////using namespace std;
+////
+////void Hello(); // 인사
+////// 메인 들어가기 전에 확인하는 기능
+////
+////int main() {
+////	Hello();	// 함수 호출
+////
+////}
+////
+////
+////void Hello() {
+////	cout << "안녕~!" << endl;
+////
+////}
+//
+//// =======================================================
+//
+//// 매개변수 함수 포함 / 매개변수 활용 가능
+//
+//// 함수 포인터 사용
+//// -> 원래 데이터의 리턴 타입과 매개변수 타입 일치 시켜 줘야 한다.
+//// -> 리턴 타입(포인터 주소) (매개변수 타입);
+//
 //#include <iostream>
 //using namespace std;
 //
-//// namespace 를 통해서 별도의 물리적인 공간을 만들어서 해당 변수가 개입하지 못하도록 하기
-//// 다른 폴더를 만들어서 같은 이름의 PASS, FAIL 이어도 충돌이 나지 않도록 하는 것
+//void Hello(int n); // 인사
+//int Factorial(int f);	// 팩토리얼 함수, 반복문 이용
+//int FactorialRecursion(int); // 팩토리얼 함수, 재귀함수 이용
 //
-//namespace sophomore {
-//	enum Java { PASS, FAIL };
-//}
-//
-//namespace freshman {
-//	enum Python { PASS, FAIL };
-//}
+//// 정의해준대로 사용하기
 //
 //int main() {
+//	int(*pFact)(int);
+//	// int 타입 (pFact 의 포인터) (int 타입)
+//	void(*pHi)(int);	//Hello 함수를 위한 배개면수 함수
 //
+//	// pFact = &Factorial;
+//	// pFact = &Hello; // Hello 의 리턴, 매개변수 타입 일치하지 않아서 오류남
 //
+//	pFact = &Factorial;
+//	pHi = &Hello;
+//
+//	int in;
+//	cin >> in;
+//
+//	cout << (*pFact)(in) << endl;
+//	(*pHi)(in);
+//	
+//
+//	cout << FactorialRecursion(in) << endl;
+//	cout << Factorial(in) << endl;
+//	Hello(2);	// 함수 호출
 //
 //}
-
-// =========================================================================================
-//
-//// Modern Style(enum class c++11)
-//
-//#include <iostream>
-//using namespace std;
-//
-//// enum class 로 작성해서 사용 가능
-//// namespace 를 사용하면 폴더를 새로 만들어야 하는 반면 enum class 는 간단하게 사용 가능
-//
-//// enum 까지 작성했을 때는 묵시적 변환이 이루어지는 등 약한 결합이 나타난다 (정수형 타입의)
-//// 그러나 enum class 로 묶게 되면 강한 결합이 이루어져 변환이 불가능하다 
-//// => enum 묶으면 case 0 : 사용 가능 /// enum class 묶으면 case 0 : 사용 불가!  => case Cplusplus::PASS : 형식으로 작성해야함
-//// enum class -> 반드시 열거형 타입의 변수 이용해야 한다!
-//// 암시적, 묵시적 변환을 막을 수 있다.
-//
-//enum class Java { PASS, FAIL };
-//enum class Python { PASS, FAIL };
-//enum class Cplusplus { PASS, FAIL };
 //
 //
-//int main() {
-//	Cplusplus c = Cplusplus::PASS;
+//void Hello(int n) {
+//	cout << "안녕~!" << " " << n << endl;
 //
-//	switch (c) {
-//		case 0 :
-//			cout << "합격!";
-//			break;
-//		case Cplusplus::FAIL :
-//			cout << " 재수강!";
+//}
+//
+//int Factorial(int f) {
+//	int result = 1;
+//
+//	for (int i = 1; i <= f; i++) {
+//		result = result * i;
+//
 //	}
-//
-//
+//	return result;
 //}
-
-// =========================================================================================
-
-// reference (변수의 별명)
-// 변수가 이름을 하나 더 가짐
-// 포인터와 유사한 파생형, 차이는 발생
-// 자료타입& 변수이름 = 대상변수;	// => 복사 생성자 (const Cicle& c);
-// const 변수 / 변수 const / const 변수 const => 방식과 동일
-
-// 레퍼런스는 별도의 메모리 할당을 받지 않고 참조만 한다.
-// 포인터는 별도의 메모리를 할당 받아서 실행 시킨다.
-
-// rule1. 반드시 선언하는 시점에 초기화 되어야 한다.
-// rule2. 처음 정의 외에는 다른 참조 사용 불가
 //
-//
-//#include <iostream>
-//using namespace std;
-//
-//int main() {
-//	// 자료타입& 변수이름 = 대상변수;
-//	//short s = 5;
-//
-//	//short& rs = s;	// 레퍼런스 변수
-//	//short* ps = &s; // 포인터
-//
-//	//cout << s << " " << &s << endl;		// 5 00000020CB2FFB54
-//	//cout << rs << " " << &rs << endl;	// 5 00000020CB2FFB54	// s와 주소 동일
-//	//cout << *ps << " " << &ps << endl;	// 5 00000020CB2FFB98
-//
-//	//int a = 1; 
-//	//int b = 2;
-//	//int& c = a;	// rule 1 (초기화)	
-//
-//	//c = 3;
-//	//c = b;	// rule 2	// b를 참조 하는 것이 아니라 b의 값을 대입한다.
-//	//c = 4;
-//
-//	//cout << a << endl;
+//int FactorialRecursion(int f){
+//	// f = 4
+//	// f = 3
+//	// f = 2
+//	// f = 1
+//	if (f <= 1) {
+//		return 1;
+//	}
+//	else {
+//		return f * FactorialRecursion(f - 1);
+//		// 4 * FactorialRecursion(3)
+//		// 3 * FactorialRecursion(2)
+//		// 2 * FactorialRecursion(1)
 //		
-//	/// [c의 변화] int& c = a;    c = 3;    c = b;    c = 4;
-//	///              1      ->      3    ->   2   ->    4
-//	/// [a의 변화] int& c = a;    c = 3;    c = b;    c = 4;
-//	///              1      ->      3    ->   2   ->    4
-//	/// [b의 변화]                          c = b;    c = 4;
-//	///                                       2   ->    2      <- c 값 변경에 따른 영향 x
-//
-//	// =========================================================================================
-//
-//	//// const 레퍼런스 변수 : 값 변경 X
-//	////					   : 상수 초기화 가능
-//	//// 함수와 함께 사용할때 시너지 효과 up!
-//
-//	//int a = 1;
-//	//int b = 2;
-//	//const int& c = a;	// rule 1 (초기화)	
-//
-//	//// c = 3;	// 오류 -> const 레퍼런스 변수는 변경 불가
-//	//// c = b;	// 오류 -> const 레퍼런스 변수는 변경 불가
-//	//// c = 4;	// 오류 -> const 레퍼런스 변수는 변경 불가
-//
-//	//cout << c << endl;
-//	//
-//	//a = 5; 
-//
-//	//cout << c << endl;	// 참조는 계속 하므로 참조하고 있던 a 가 바뀌면 c 참조값도 변경되어 나옴
-//
-//	int a = 5;
-//	// int& ra = 9;		// 불가
-//	const int& ra = 9;	// 가능 ( 9 참조 )	->  수정 불가
-//
-//	cout << ra << endl;
-//
-//	// 레퍼런스 변수의 대상 변수는 변수 이름이 와야 정상
-//	// 그러나 const 를 붙이면 값으로 대상 변수 역할 가능	
+//	}
 //
 //}
